445f51dfa468f5550325c540e4be961b4a8fd224
Robert Adams <adamsiii.robert@gmail.com>
2023-02-12 04:24:53 -0800

    Add triangle

---
+++ src/lib/Triangle/index.ts
@@ -0,0 +1,14 @@
+import { Shape } from "../Shape";
+
+export class Triangle extends Shape {
+  public shape_color: string;
+
+  constructor(text: string, text_color: string, shape_color: string) {
+    super(text, text_color);
+    this.shape_color = shape_color;
+  }
+
+  render() {
+    return `<polygon points="100 0, 0 200, 200 200" fill='${this.shape_color}'/>`;
+  }
+}
