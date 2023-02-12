0482e2f8fac6124926b147273603e82e6e1c36d4
Robert Adams <adamsiii.robert@gmail.com>
2023-02-12 05:04:52 -0800

    Add trim

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -4,7 +4,7 @@ import { COLOR_MAP } from "./colors/COLOR_MAP";
 export const VALID_HEX = /(^#[0-9A-F]{3}$)|(^#[0-9A-F]{6}$)/im;
 
 export const validateColor = (input: string) => {
-  if (input in COLOR_MAP || VALID_HEX.test(input)) {
+  if (input.trim() in COLOR_MAP || VALID_HEX.test(input)) {
     return true;
   }
   return "Please enter a valid color";
