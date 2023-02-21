af934c02c8317881016bfc2b2791e61857be34dc
Robert Adams <adamsiii.robert@gmail.com>
2023-02-21 09:46:22 -0800

    Moved validators to App dir

--- src/App/validators.ts
+++ src/App/validators.ts
@@ -1,14 +1,7 @@
-import { COLOR_MAP } from "../colors/COLOR_MAP";
+import { COLOR_MAP } from "../utils/colors/COLOR_MAP";
 
 // #YYY, #YYYYYY
 export const VALID_HEX = /(^#[0-9A-F]{3}$)|(^#[0-9A-F]{6}$)/im;
-
-export const validateColor = (input: string) => {
-  if (input.trim() in COLOR_MAP || VALID_HEX.test(input)) {
-    return true;
-  }
-  return "Please enter a valid color";
-};
 
 export const validateMonogram = (input: string) => {
   const text = input.trim();
@@ -18,4 +11,11 @@ export const validateMonogram = (input: string) => {
     return "Please enter a maximum of 3 characters";
   }
   return true;
+};
+
+export const validateColor = (input: string) => {
+  if (input.trim() in COLOR_MAP || VALID_HEX.test(input)) {
+    return true;
+  }
+  return "Please enter a valid color";
 };
