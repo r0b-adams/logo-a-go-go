aa0baa73caa2388d1cb4df4ebd01016ddf6adfe5
Robert Adams <adamsiii.robert@gmail.com>
2023-02-21 09:47:01 -0800

    Refactored logic to App static class

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,3 @@
-import fs from "fs";
-import { enterMonogram, selectColor, selectShape } from "./utils/prompts";
+import App from "./App";
 
-const DIST = "examples";
-
-const createOutputDir = (path: string) => {
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
-  }
-};
-
-const main = async () => {
-  createOutputDir(DIST);
-
-  const { text } = await enterMonogram();
-  const { text_color } = await selectColor("text_color");
-  const { shape } = await selectShape();
-  const { fill_color } = await selectColor("fill_color");
-
-  const svg = new shape(text, text_color, fill_color).createSvg();
-  fs.writeFileSync(`${DIST}/logo.svg`, svg);
-  console.log("logo generated!");
-};
-
-main();
+App.main();
