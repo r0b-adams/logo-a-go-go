bc1caab91afb3b7f4de53d2ac78aeec1f7acb9f9
Robert Adams <adamsiii.robert@gmail.com>
2023-02-20 17:31:45 -0800

    Move prompt validators to own file

---
+++ src/utils/prompts/validators.ts
@@ -0,0 +1,21 @@
+import { COLOR_MAP } from "../colors/COLOR_MAP";
+
+// #YYY, #YYYYYY
+export const VALID_HEX = /(^#[0-9A-F]{3}$)|(^#[0-9A-F]{6}$)/im;
+
+export const validateColor = (input: string) => {
+  if (input.trim() in COLOR_MAP || VALID_HEX.test(input)) {
+    return true;
+  }
+  return "Please enter a valid color";
+};
+
+export const validateMonogram = (input: string) => {
+  const text = input.trim();
+  if (!text) {
+    return "Please enter up to 3 characters";
+  } else if (text.length > 3) {
+    return "Please enter a maximum of 3 characters";
+  }
+  return true;
+};
