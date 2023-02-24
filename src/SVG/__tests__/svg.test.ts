82264e1661d08f88e032a6fe77c59a7a2b2f90f2
Robert Adams <adamsiii.robert@gmail.com>
2023-02-24 09:00:46 -0800

    Add SVG test boilerplate

---
+++ src/SVG/__tests__/svg.test.ts
@@ -0,0 +1,8 @@
+describe("SVG", () => {
+  it("has dimensions width 300px and height 200px", () => {});
+  it("sets and gets text element template", () => {});
+  it("sets and gets shape element template shape", () => {});
+  it("throws exception if render called with no text", () => {});
+  it("throws exception if render called with no shape", () => {});
+  it("renders an svg template with given parameters", () => {});
+});
